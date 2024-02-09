67fe772547b3415b3da5b3d83c4a6fec6eb15d0f
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-02-09 10:12:10 +0700

    Update lineage_RMX3063.mk

--- lineage_RMX3063.mk
+++ lineage_RMX3063.mk
@@ -20,8 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX3063/device.mk)
-
-# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/realme/RMX3063/RMX3063-vendor.mk)
 
 # Inherit some common LineageOS stuff.
