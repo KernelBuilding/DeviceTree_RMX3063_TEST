71d4b34a2c36ba36e359982c88ca42c0ec8db6c0
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-02-18 14:29:50 +0700

    Update lineage_RMX3063.mk

--- lineage_RMX3063.mk
+++ lineage_RMX3063.mk
@@ -20,10 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX3063/device.mk)
-$(call inherit-product, vendor/realme/RMX3063/RMX3063-vendor.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_RMX3063
@@ -32,7 +31,11 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme C20
 PRODUCT_MANUFACTURER := realme
 
-# Bootanimation
+# Sakura build flags
+SAKURA_OFFICIAL := false
+SAKURA_MAINTAINER := PGYT PROJECT
+SAKURA_BUILD_TYPE := vanilla
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Build info
