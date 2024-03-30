b630cc008ef0433501e8c0afb958a4b90d302567
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-03-30 15:25:41 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX3063.mk
+    $(LOCAL_DIR)/fluid_RMX3063.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX3063-user \
-    arrow_RMX3063-userdebug \
-    arrow_RMX3063-eng
+    fluid_RMX3063-user \
+    fluid_RMX3063-userdebug \
+    fluid_RMX3063-eng
